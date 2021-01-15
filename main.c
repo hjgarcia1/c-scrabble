b7be87ddc91ac79ececade348d76421f68fbb5d3
Henry J. Garcia <hjgarcia1@gmail.com>
2021-01-15 00:16:03 -0500

    final solution

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 #include <string.h>
 #include <stdlib.h>
 
-int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+int letter_value[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 char *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 int calculate_points(char *word)
 {
-    int i, j, k, player_points = 0;
+    int i, j, k, points = 0;
 
     for (i = 0; i < strlen(word); i++)
     {
@@ -16,13 +16,13 @@ int calculate_points(char *word)
         {
             if (word[i] == alphabet[j])
             {
-                player_points += points[j];
+                points += letter_value[j];
                 break;
             }
         }
     }
 
-    return player_points;
+    return points;
 }
 
 void get_winner(int player_one, int player_two)
@@ -46,35 +46,37 @@ int main(int argc, char *argv[])
     //A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q   R  S  T  U  V  W  X  Y  Z
     //1  3  3  2  1  4  2  4  1  8  5  1  3  1  1  3  10  1  1  1  1  4  4  8  4  10
 
-    int player_one = 0,
-        player_two = 0, 
+    int p1 = 0,
+        p2 = 0,
         i = 0, j = 0;
 
+    char p1_word[32], p2_word[32];
 
-    char player_one_word[] = "qi", player_two_word[] = "code", up_case_word_one[10], up_case_word_two[10];
+    printf("Player One's word: ");
+    scanf("%s", &p1_word);
+    printf("Player Two's word: ");
+    scanf("%s", &p2_word);
 
-    // Loop
-    while (player_one_word[i])
+    while (p1_word[i])
     {
-        up_case_word_one[i] = toupper(player_one_word[i]);
+        p1_word[i] = toupper(p1_word[i]);
         i++;
     }
 
-    j = 0;
-    while (player_two_word[j])
+    while (p2_word[j])
     {
-        up_case_word_two[j] = toupper(player_two_word[j]);
+        p2_word[j] = toupper(p2_word[j]);
         j++;
     }
 
-    player_one = calculate_points(up_case_word_one);
+    p1 = calculate_points(p1_word);
 
-    player_two = calculate_points(up_case_word_two);
+    p2 = calculate_points(p2_word);
 
-    printf("Player One's word: '%s'is worth %i\n", player_one_word, player_one);
-    printf("Player Two's word: '%s'is worth %i\n", player_two_word, player_two);
+    printf("Player One's word: '%s' is worth %i\n", p1_word, p1);
+    printf("Player Two's word: '%s' is worth %i\n", p2_word, p2);
 
-    get_winner(player_one, player_two);
+    get_winner(p1, p2);
 
     return 0;
 }
