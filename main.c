2e3d4f8540ce45797bdb8c0cd47c7827df98b348
Henry J. Garcia <hjgarcia1@gmail.com>
2021-01-14 22:54:07 -0500

    words are now uppercased

--- main.c
+++ main.c
@@ -6,56 +6,32 @@
 int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 char *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-int main(int argc, char *argv[])
+int calculate_points(char *word)
 {
-    //A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q   R  S  T  U  V  W  X  Y  Z
-    //1  3  3  2  1  4  2  4  1  8  5  1  3  1  1  3  10  1  1  1  1  4  4  8  4  10
+    int i, j, k, player_points = 0;
 
-    int player_one_points = 0,
-        player_two_points = 0,
-        i, j;
-
-    char *player_one_word, *player_two_word;
-
-    //set the words
-    player_one_word = "CODE";
-
-    //get points for player one
-    for (i = 0; i < strlen(player_one_word); i++)
+    for (i = 0; i < strlen(word); i++)
     {
         for (j = 0; j < strlen(alphabet); j++)
         {
-            if (player_one_word[i] == alphabet[j])
+            if (word[i] == alphabet[j])
             {
-                player_one_points += points[j];
+                player_points += points[j];
                 break;
             }
         }
     }
 
-    //get points for player two
-    player_two_word = "BEE";
+    return player_points;
+}
 
-    for (i = 0; i < strlen(player_two_word); i++)
-    {
-        for (j = 0; j < strlen(alphabet); j++)
-        {
-            if (player_two_word[i] == alphabet[j])
-            {
-                player_two_points += points[j];
-                break;
-            }
-        }
-    }
-
-    printf("Player one's word: '%s'is worth %i\n", player_one_word, player_one_points);
-    printf("Player two's word: '%s'is worth %i\n", player_two_word, player_two_points);
-
-    if (player_one_points == player_two_points)
+void get_winner(int player_one, int player_two)
+{
+    if (player_one == player_two)
     {
         printf("Tied Game.\n");
     }
-    else if (player_one_points > player_two_points)
+    else if (player_one > player_two)
     {
         printf("Player One Wins Game.\n");
     }
@@ -63,6 +39,42 @@ int main(int argc, char *argv[])
     {
         printf("Player Two Wins Game.\n");
     }
+}
+
+int main(int argc, char *argv[])
+{
+    //A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q   R  S  T  U  V  W  X  Y  Z
+    //1  3  3  2  1  4  2  4  1  8  5  1  3  1  1  3  10  1  1  1  1  4  4  8  4  10
+
+    int player_one = 0,
+        player_two = 0, 
+        i = 0, j = 0;
+
+
+    char player_one_word[] = "qi", player_two_word[] = "code", up_case_word_one[10], up_case_word_two[10];
+
+    // Loop
+    while (player_one_word[i])
+    {
+        up_case_word_one[i] = toupper(player_one_word[i]);
+        i++;
+    }
+
+    j = 0;
+    while (player_two_word[j])
+    {
+        up_case_word_two[j] = toupper(player_two_word[j]);
+        j++;
+    }
+
+    player_one = calculate_points(up_case_word_one);
+
+    player_two = calculate_points(up_case_word_two);
+
+    printf("Player One's word: '%s'is worth %i\n", player_one_word, player_one);
+    printf("Player Two's word: '%s'is worth %i\n", player_two_word, player_two);
+
+    get_winner(player_one, player_two);
 
     return 0;
 }
